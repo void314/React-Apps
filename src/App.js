25af745bd111d48e37769b36699d3abc232074f6
void314 <artiemdrozdov@yandex.kz>
2022-11-20 20:15:02 +0600

    counter commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
+
+  const [count, setCount] = React.useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='counter-wrapper'>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count - 1)} className="counter-minus">- Минус</button>
+        <button onClick={() => setCount(count + 1)} className="counter-plus">Плюс +</button>
+      </div>
+      <button onClick={() => setCount(0)} className="counter-erase">X</button>
     </div>
   );
 }
